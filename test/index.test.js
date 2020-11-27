f26f9f22d0fa36b16a66f7387e4e616b98a63976
Sergey Zelenov <mail@zelenov.su>
2020-11-27 18:44:57 +0100

    koa application

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,19 @@
-const sum = require('../index');
 const assert = require('assert');
+const axios = require('axios');
+const app = require('../app');
 
-describe('function sum', () => {
-  it('should sum 2 numbers', () => {
-    assert.strictEqual(sum(1, 2), 3);
+describe('app tests', () => {
+  let server;
+  before((done) => {
+    server = app.listen(3000, done);
+  });
+
+  after((done) => {
+    server.close(done);
+  });
+
+  it('should respond with "hello"', async () => {
+    const response = await axios.get('http://localhost:3000');
+    assert.strictEqual(response.data, 'hello');
   });
 });
